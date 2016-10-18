5533c88fad256e730abf40451285ed59c87aa63c
MorttiBoi <niko_hyytia@hotmail.com>
2016-10-18 18:47:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekstiseikkailu2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tekstiseikkailu2!!!");
+            Console.WriteLine("asdasd");
+            Console.WriteLine("Tein työtä!");
+            Console.ReadKey();
+        }
+    }
+}
